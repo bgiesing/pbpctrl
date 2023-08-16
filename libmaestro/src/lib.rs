83d521efa4ac93d4575ac967d8b857f093e0f3f4
Brandon Giesing <me@brandongiesing.com>
2023-08-15 23:44:18 -0500

    Test: Pixel Buds A-Series UUID

--- libmaestro/src/lib.rs
+++ libmaestro/src/lib.rs
@@ -7,7 +7,7 @@ use uuid::{uuid, Uuid};
 /// UUID under which the Maestro protocol is advertised.
 ///
 /// Defined as `25e97ff7-24ce-4c4c-8951-f764a708f7b5`.
-pub const UUID: Uuid = uuid!("25e97ff7-24ce-4c4c-8951-f764a708f7b5");
+pub const UUID: Uuid = uuid!("3a046f6d-24d2-7655-6534-0d7ecb759709");
 
 pub mod hdlc;
 pub mod protocol;
